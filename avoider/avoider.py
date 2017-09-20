a95e13ca182c598d2e4c57dc4023baf1548d7d57
Kirk Roerig <mr.possoms@gmail.com>
2017-09-20 16:27:41 -0400

    Got TF weirdness somewhat straightened out

--- avoider/avoider.py
+++ avoider/avoider.py
@@ -13,18 +13,18 @@ SAMPLE_SIZE = None
 
 
 class BlobTrainingSet():
-    def __init__(self, path, shape=[160, 120]):
+    def __init__(self, path, shape=[160, 120, 1]):
         self.index = 0
         self.file = open(path, mode='rb')
         self.shape = shape
-        self._sample_size = None;
 
         self.magic = struct.unpack('Q', self.file.read(8))
-        self.is_raw = self.file.read(1);
+        self.is_raw = self.file.read(8);
+        self.data_start = self.file.tell()
 
     def reset(self):
         self.index = 0
-        self.file.seek(9);
+        self.file.seek(self.data_start, 0);
 
     def size(self):
         last_pos = self.file.tell()
@@ -50,35 +50,13 @@ class BlobTrainingSet():
         stop = self.file.tell()
 
         print(stop - start)
+        state = (np.frombuffer(luma, dtype=np.uint8).reshape(np.prod(self.shape)) / 255.0)
 
-        return luma
+        return np.asarray(state , dtype=np.float32).reshape([1, np.prod(self.shape)]), \
+               np.asarray(action_vector, dtype=np.float32).reshape([1, 22])
 
     def next_batch(self, size):
-        images, labels = [], []
-
-
-        # coord = tf.train.Coordinator()
-        # threads = tf.train.start_queue_runners(coord=coord)
-
-        for _ in range(size):
-            buf = self.file.read(np.prod(self.shape))
-            tag = struct.unpack('I', self.file.read(4))[0]
-
-            assert tag is 0 or tag is 1
-
-            images += [ np.frombuffer(buf, dtype=np.uint8).reshape(np.prod(self.shape)) / 255.0 ]
-            # Image.frombytes('L', (128, 128), buf).show()
-            if tag == 1:
-                labels += [1.0]
-            else:
-                labels += [-1.0]
-
-        self.index += size
-        # coord.request_stop()
-        # coord.join(threads)
-
-        batch = np.asarray(images), np.asarray(labels).reshape([len(labels), 1])
-        return batch
+        return None
         #return np.asarray(images), np.asarray(labels)
 
 
@@ -91,14 +69,15 @@ def sample_size():
 
 class AvoiderNet:
     def __init__(self):
-        self.x = x = tf.placeholder(tf.float32, shape=[None, 19200])
-        self.y_ = tf.placeholder(tf.float32, shape=[None, 22])
+        self.x = x = tf.placeholder(tf.float32, shape=[None, 19200], name='X')
+        self.y_ = tf.placeholder(tf.float32, shape=[None, 22], name='y_labels')
 
         self.x_image = x_image = tf.reshape(x, [-1, 160, 120, 1])
-        self.conv_layers = (image_input(x_image, [5, 5, 1])
+        self.conv_layers = (image_input(x_image, [160, 120, 1])
                             .pool(2)
                             .to_conv(filter=[5, 5, 8])
-                            .pool(2))
+                            .pool(2)
+                           )
 
         self.h = self.conv_layers.to_fc(1024).output_vector(22)
         cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.y_, logits=self.h))
@@ -108,23 +87,32 @@ class AvoiderNet:
         self.prediction_accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
     def train(self, x, y_):
-        self.training_step.run(feed_dict={x: x, y_: y_})
+        self.training_step.run(feed_dict={"X:0": x, "y_labels:0": y_})
 
     def predict(self, x):
         return self.h.eval(feed_dict={x: x})
 
     def accuracy(self, x, y_):
-        self.prediction_accuracy.eval(feed_dict={x: x, y_: y_})
+        self.prediction_accuracy.eval(feed_dict={"X": x, "y_labels": y_})
 
 
 
 def main(args):
-    with tf.Session() as session:
-        net = AvoiderNet()
+    net = AvoiderNet()
 
     ts = BlobTrainingSet(path="s0")
+    ts.reset()
     ts.decode_sample()
     ts.decode_sample()
+    ts.decode_sample()
+
+    with tf.Session() as session:
+        session.run(tf.global_variables_initializer())
+        x, y_ = ts.decode_sample()
+        net.train(x, y_)
+
+
+    print(x, y_)
 
 if __name__ == '__main__':
     main(sys.argv)
