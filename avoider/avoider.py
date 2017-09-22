172f0abd887494f3d9e9b6bcbfe81b0d707a3b4a
Kirk Roerig <mr.possoms@gmail.com>
2017-09-22 00:04:26 -0400

    Avoider maybe sorta working

--- avoider/avoider.py
+++ avoider/avoider.py
@@ -1,7 +1,8 @@
+#!/Users/kirk/.pyenv/shims/python
 import tensorflow as tf
 import struct
 import sys
-import os
+import getopt
 import subprocess
 from random import shuffle
 from fibers.fibers import *
@@ -15,23 +16,37 @@ SAMPLE_SIZE = None
 
 
 class BlobTrainingSet():
-    def __init__(self, path, shape=[160, 120, 1]):
+    def __init__(self, path=None, shape=[160, 120, 1], file=None):
         self.index = 0
-        self.file = open(path, mode='rb')
+        self.is_stream = False
+
+        if file is not None:
+            self.file = file
+            self.is_stream = True
+        elif path is not None:
+            self.file = open(path, mode='rb')
+
         self.shape = shape
 
         self.magic = struct.unpack('Q', self.file.read(8))
-        self.is_raw = self.file.read(8);
-        self.data_start = self.file.tell()
+        self.is_raw = struct.unpack('Q', self.file.read(8))
+
+        print('magic: %d' % self.magic)
+        print('raw: %d' % self.is_raw)
+
+        if self.is_stream is False:
+            self.data_start = self.file.tell()
 
         self.reset()
 
-
-
     def seek_sample(self, sample_index):
-        self.file.seek(self.data_start + sample_size() * sample_index, 0)
+        if self.is_stream:
+            self.file.seek(self.data_start + sample_size() * sample_index, 0)
 
     def reset(self):
+        if self.is_stream:
+            return
+
         self.order = list(range(0, self.size() - 1))
         shuffle(self.order)
         self.index = 0
@@ -67,9 +82,11 @@ class BlobTrainingSet():
 
         samples, labels = [], []
         added = 0
-        while(added < size and len(self.order) > 0):
-            i = self.order.pop()
-            self.seek_sample(i)
+        while(added < size and (self.is_stream or len(self.order) > 0)):
+            if not self.is_stream:
+                i = self.order.pop()
+                self.seek_sample(i)
+
             x, y = self.decode_sample()
 
             samples.append(x)
@@ -100,13 +117,14 @@ class AvoiderNet:
                            )
 
         self.h = self.conv_layers.to_fc(1024).output_vector(22)
-        cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.y_, logits=self.h))
-        self.training_step = tf.train.AdamOptimizer(1E-4).minimize(cross_entropy)
+        self.cost = tf.reduce_mean(tf.pow(self.h - self.y_, 2))
+        self.training_step = tf.train.AdamOptimizer(1E-4).minimize(self.cost)
 
-        correct_prediction = tf.equal(tf.argmax(self.h, 1), tf.argmax(self.y_, 1))
+        correct_prediction = self.cost < 1
         self.prediction_accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
     def train(self, x, y_):
+        print(self.cost.eval(feed_dict={"X:0": x, "y_labels:0": y_}))
         self.training_step.run(feed_dict={"X:0": x, "y_labels:0": y_})
 
     def predict(self, x):
@@ -119,33 +137,84 @@ class AvoiderNet:
 
 def main(args):
     model_path = "/tmp/avoider.ckpt"
+
+    opts, _ = getopt.getopt(args, 'sp')
+    ts = None
+    predict = False
+    use_stdin = False
+
+    for k, v in opts:
+        if 's' in k:
+            use_stdin = True
+
+        if 'p' in k:
+            predict = True
+
     net = AvoiderNet()
-    ts = BlobTrainingSet(path="s0")
     saver = tf.train.Saver()
 
+    if use_stdin:
+        ts = BlobTrainingSet(file=sys.stdin.buffer)
+    else:
+        ts = BlobTrainingSet(path="s0")
+
     with tf.Session() as session:
-        saver.restore(session, model_path)
+        if predict:
+            print('Predicting output')
+            try:
+                saver.restore(session, model_path)
+            except tf.errors.NotFoundError:
+                print('Failed to load')
+        else:
+            print('Training')
 
         session.run(tf.global_variables_initializer())
 
-        epoch = 0
-        while(len(ts.order) > 0):
-            x, y_ = ts.next_batch(50)
-            net.train(x, y_)
-            epoch += 1
+        if predict:
+            while True:
+                x, y_ = ts.next_batch(1)
 
-            print(len(ts.order))
-            if epoch % 10 == 0:
-                saver.save(session, model_path)
+                h = net.predict(x).reshape([22])
+                i = 0
 
-        ts.reset()
-        x, y_ = ts.next_batch(1000)
-        print(net.accuracy(x, y_))
+                print('-------------------------------')
+                for f in h:
+                    i += 1
+                    if i < 7: continue
+                    str = ''
+                    for _ in range(0, int(f * 10)):
+                        str += '#'
 
-        ts.reset()
-        x, y_ = ts.next_batch(1)
-        print(net.predict(x))
-        print(y_)
+                    print(str)
+
+        else:
+            for epoch in range(1, 50):
+                ts.reset()
+                while len(ts.order) > 0:
+                    x, y_ = ts.next_batch(1000)
+                    net.train(x, y_)
+
+                    print(len(ts.order))
+                    if epoch % 10 == 0:
+                        saver.save(session, model_path)
+
+                ts.reset()
+                x, y_ = ts.next_batch(1000)
+                print('accuracy: %f' % net.accuracy(x, y_))
+
+            ts.reset()
+            x, y_ = ts.next_batch(1000)
+            print('accuracy: %f' % net.accuracy(x, y_))
+
+            ts.reset()
+            x, y_ = ts.next_batch(1)
+            h = net.predict(x)
+            print("h: %s" % h)
+            print("y_: %s" % y_)
+
+            avg = np.average((h - y_), 1)
+
+            print(avg)
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main(sys.argv[1:])
