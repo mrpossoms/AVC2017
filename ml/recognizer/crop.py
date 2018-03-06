4f2384a347c5a1034595c8e6a11104f0e2902953
Kirk Roerig <mr.possoms@gmail.com>
2018-03-06 00:34:35 -0500

    Added shifted crop

--- ml/recognizer/crop.py
+++ ml/recognizer/crop.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 
 from PIL import Image
+import random
+import string
 import sys
 import io
+
+def name():
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
 
 str_buf = ''
 #for unit in sys.stdin.read(1):
@@ -12,5 +17,13 @@ str_buf = ''
 #buf = io.BytesIO(sys.stdin.buffer)
 
 
+
 img = Image.open(sys.stdin.buffer)
-img.crop([0, 0, 64, 64]).save(sys.argv[1], 'PNG')
+
+w, h = 64, 64
+
+for _ in range(6):
+    x_range = img.width - w
+    y_range = img.height - h
+    x, y = int(random.random() * x_range), int(random.random() * y_range)
+    img.crop([x, y, x + 64, y + 64]).save('%s%s' % (sys.argv[1], name()), 'PNG')
