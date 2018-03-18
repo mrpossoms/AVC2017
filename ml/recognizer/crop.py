01a20078a19ff5de240b10282959a73ca8999cd4
Kirk Roerig <mr.possoms@gmail.com>
2018-03-18 01:38:19 -0400

    Good enough

--- ml/recognizer/crop.py
+++ ml/recognizer/crop.py
@@ -1,10 +1,15 @@
-#!/usr/bin/python3
-
+#!/Users/kirk/.pyenv/shims/python3
 from PIL import Image
 import random
 import string
 import sys
 import io
+import os
+import time
+
+random.seed(time.time())
+
+print(">>>>>")
 
 def name():
     return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
@@ -17,13 +22,25 @@ str_buf = ''
 #buf = io.BytesIO(sys.stdin.buffer)
 
 
+os.makedirs('imgs/{}'.format(sys.argv[1]), exist_ok=True)
+i = 0
 
-img = Image.open(sys.stdin.buffer)
+while True:
+    line = sys.stdin.readline()
+    path = line.rstrip()
 
-w, h = 64, 64
+    if len(path) == 0:
+        break
 
-for _ in range(6):
-    x_range = img.width - w
-    y_range = img.height - h
-    x, y = int(random.random() * x_range), int(random.random() * y_range)
-    img.crop([x, y, x + 64, y + 64]).save('%s%s' % (sys.argv[1], name()), 'PNG')
+    print('[[' + path + ']]')
+    img = Image.open(path)
+
+    w, h = 64, 64
+
+    i += 1
+
+    for _ in range(6):
+        x_range = img.width - w
+        y_range = img.height - h
+        x, y = int(random.random() * x_range), int(random.random() * y_range)
+        img.crop([x, y, x + 64, y + 64]).save('imgs/%s/%s' % (sys.argv[1], name()), 'PNG')
--- ml/recognizer/dl.sh
+++ ml/recognizer/dl.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 i=0
-mkdir -p $1
 read img_path
 while [ $? -eq 0 ]
 do
 	if [ -z $1 ]
 	then
 		curl -G $img_path
+		#cat /tmp/img
 	else
-		wget -bO $1/$i -q $img_path
+		mkdir -p $1
+		wget -O $1/$i -q $img_path
+		echo $1/$i
 	fi
 ((i++))
 read img_path
