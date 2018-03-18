2ad96f8c450cb38740b829f26cfe0fc4449938f7
Kirk Roerig <mr.possoms@gmail.com>
2018-03-18 18:58:58 -0400

    Training up to ~84% on training set, ~78% on the test set. Not bad considering I think the data from google image is not ideal

--- ml/recognizer/recog-trainer.py
+++ ml/recognizer/recog-trainer.py
@@ -19,37 +19,68 @@ def filenames_labels():
 
     return files_labels
 
+
+def real_test_filenames_labels():
+    return [
+        ('imgs/real/real0', 1),
+        ('imgs/real/real1', 1)
+    ]
+
+
+def array_from_file(path):
+    img = Image.open(path).convert('RGB')
+    return np.array(img)
+
+
 def show_example(X, i, title=None):
     test = Image.fromarray(((X[i] + 0.5) * 256).reshape((32, 32, 3)).astype(np.uint8))
     test.show(title=title)
 
+
 def minibatch(files_labels, index, size=100):
     X, Y = [], []
-    for file, label in files_labels[index * size: index * size + size]:
-        img = Image.open(file).convert('RGB')
-        img_array = np.array(img)
 
+    for file, label in files_labels[index * size: index * size + size]:
+        img_array = array_from_file(file)
         assert(img_array.size == 3 * 32**2)
 
         X += [img_array.flatten()]
         Y += [[label]]
 
-    return (np.array(X).reshape((size, 3072)) / 256.0 - 0.5), np.array(Y).reshape((size, 1))
+    m = len(X)
 
-ts = filenames_labels()
-m_b = len(ts) // 100
+    return (np.array(X).reshape((m, 3072)) / 256.0 - 0.5), np.array(Y).reshape((m, 1))
 
-model = nn.MLPClassifier(learning_rate_init=0.0001, solver='adam', max_iter=2, warm_start=True)
 
-for i in range(m_b):
-    X, Y = minibatch(ts, i)
-    model.fit(X, Y)
+# Get the set of all the labels and file paths, pre shuffled
+full_set = filenames_labels()
 
-    if i % 10 == 0:
-        print(model.score(X, Y))
+# find total number of samples, compute count of complete batches
+m = len(full_set)
+batches = m // 100
 
-ts_X, ts_Y = minibatch(ts, 20, size=10)
-print(model.score(ts_X, ts_Y))
+# Split in to a training and test set
+ts_batches = int(np.floor(batches * 0.75)) - 1
+training_set = full_set[0:ts_batches * 100]
+test_set = full_set[ts_batches * 100:-1]
+
+model = nn.MLPClassifier(hidden_layer_sizes=[256, 256, 256],
+                         learning_rate_init=0.0001,
+                         alpha=0.001,
+                         solver='adam',
+                         max_iter=2,
+                         warm_start=True)
+
+for _ in range(2):
+    for i in range(ts_batches):
+        X, Y = minibatch(training_set, i)
+        model.fit(X, Y)
+
+        if i % 10 == 0:
+            print(model.score(X, Y))
+
+ts_X, ts_Y = minibatch(test_set, 0, size=100)
+print('Test set score: %f' % model.score(ts_X, ts_Y))
 
 y_ = model.predict(ts_X)
 
@@ -75,3 +106,7 @@ for i in range(y_.size):
 
     if shown_positive and shown_negative:
         break
+
+X, Y = minibatch(real_test_filenames_labels(), 0, size=2)
+print('Real set score: %f' % model.score(X, Y))
+print(model.predict(X))
