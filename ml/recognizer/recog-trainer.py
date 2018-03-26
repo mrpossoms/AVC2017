74ace41dee0280abc0d9c0e2aa2ea3a10283219f
Kirk Roerig <kirk@kirkroerig.com>
2018-03-26 08:55:50 -0400

    Random hyper parameter search

--- ml/recognizer/recog-trainer.py
+++ ml/recognizer/recog-trainer.py
@@ -2,6 +2,7 @@ from sklearn import neural_network as nn
 import numpy as np
 from PIL import Image
 from random import shuffle
+import sys
 import os
 import time
 import signal
@@ -42,7 +43,6 @@ def activation_map(model, in_path, out_path):
             act_map[x, y] = patch[16, 16] * color
 
     Image.fromarray(act_map.astype(np.uint8)).save(out_path, "PNG")
-
 
 
 def filenames_labels():
@@ -94,44 +94,108 @@ def minibatch(files_labels, index, size=100):
     return (np.array(X).reshape((m, 3072)) / 256.0 - 0.5), np.array(Y).reshape((m, 3))
 
 
-# Get the set of all the labels and file paths, pre shuffled
-full_set = filenames_labels()
+def main(argv):
+    # train({
+    #     'layer_sizes': [256, 256, 256],
+    #     'learning_rate': 0.0001,
+    #     'l2_reg_term': 0.001,
+    #     'epochs': -1             # -1: only stops at SIGINT
+    # })
 
-# find total number of samples, compute count of complete batches
-m = len(full_set)
-n = 1000 # mini batch size
-batches = m // 1000
+    hyper_parameter_search({
+        'layer_sizes': ([64, 512], 1, 5),
+        'learning_rate': (0.0, 0.0001, 0.001),
+        'l2_reg_term': (0.0, 0.0001, 0.001)
+    })
 
-# Split in to a training and test set
-ts_batches = int(np.floor(batches * 0.75)) - 1
-training_set = full_set[0:ts_batches * n]
-test_set = full_set[ts_batches * n:-1]
 
-model = nn.MLPClassifier(hidden_layer_sizes=[256, 256, 256],
-                         learning_rate_init=0.0001,
-                         alpha=0.001,
-                         solver='adam',
-                         max_iter=2,
-                         warm_start=True)
+def hyper_parameter_search(ranges, candidates=100):
 
-while IS_TRAINING:
-    for i in range(ts_batches):
-        if not IS_TRAINING:
-            break
+    hyper_param_set = []
 
-        X, Y = minibatch(training_set, i, size=n)
-        model.fit(X, Y)
+    for _ in range(candidates):
+        hyper_params = {}
 
-        if i % 10 == 0:
-            print(model.score(X, Y))
+        for param in ranges:
+            t, low, high = ranges[param]
+            rnd = low + np.random.random() * (high - low)
 
-ts_X, ts_Y = minibatch(test_set, 0, size=100)
-print('Test set score: %f' % model.score(ts_X, ts_Y))
+            if type(t) is int:
+                rnd = int(rnd)
+            elif type(t) is float:
+                rnd = float(rnd)
+            elif type(t) is list:
+                layer_size = int(np.random.random() * (t[1] - t[0]) + t[0])
+                rnd = [layer_size] * int(np.ceil(rnd))
 
-activation_map(model, "test0.png", "act_map0.png")
-activation_map(model, "test1.png", "act_map1.png")
+            hyper_params[param] = rnd
 
-#X, Y = minibatch(real_test_filenames_labels(), 0, size=2)
-#print('Real set score: %f' % model.score(X, Y))
-print(model.predict(X))
+        hyper_params['epochs'] = 10
+        hyper_param_set.append(hyper_params)
+
+    i = 0
+    best = 0
+    for hp in hyper_param_set:
+        print('%d/100 evaluated' % i)
+        hp['dev_score'] = train(hp)
+        i += 1
+
+        if hp['dev_score'] > best:
+            best = hp['dev_score']
+            print(hp)
+
+
+def train(hyper_params):
+    # Get the set of all the labels and file paths, pre shuffled
+    full_set = filenames_labels()
+
+    # find total number of samples, compute count of complete batches
+    m = len(full_set)
+    n = 1000 # mini batch size
+    batches = m // 1000
+
+    # Split in to a training and test set
+    ts_batches = int(np.floor(batches * 0.75)) - 1
+    training_set = full_set[0:ts_batches * n]
+    dev_set = full_set[ts_batches * n:-1]
+
+    model = nn.MLPClassifier(hidden_layer_sizes=hyper_params['layer_sizes'],
+                             learning_rate_init=hyper_params['learning_rate'],
+                             alpha=hyper_params['l2_reg_term'],
+                             solver='adam',
+                             max_iter=2,
+                             warm_start=True)
+
+    epochs = hyper_params['epochs']
+    while IS_TRAINING or epochs == 0:
+        epochs -= 1
+        for i in range(ts_batches):
+            if not IS_TRAINING:
+                break
+
+            X, Y = minibatch(training_set, i, size=n)
+            model.fit(X, Y)
+
+            if i % 10 == 0:
+                print(model.score(X, Y))
+
+
+    ds_X, ds_Y = minibatch(dev_set, 0, size=100)
+    score = model.score(ds_X, ds_Y)
+    print('Dev set score: %f' % score)
+
+    # activation_map(model, "test0.png", "act_map0.png")
+    # activation_map(model, "test1.png", "act_map1.png")
+
+    #X, Y = minibatch(real_test_filenames_labels(), 0, size=2)
+    #print('Real set score: %f' % model.score(X, Y))
+
+    # X, Y = minibatch(real_test_filenames_labels(), 0, size=2)
+    # print('Real set score: %f' % model.score(X, Y))
+    # print(model.predict(X))
+
+    return score
+
+if __name__ == '__main__':
+    main(sys.argv)
 
