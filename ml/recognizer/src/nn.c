6a593d82fd1d1bc4a38f8c02662df092e8ec7172
Kirk Roerig <mr.possoms@gmail.com>
2018-04-10 09:40:29 -0500

    Started working on C implementation of feed forward nn

---
+++ ml/recognizer/src/nn.c
@@ -0,0 +1,90 @@
+#include "nn.h"
+#include <assert.h>
+#include <stdlib.h>
+
+static mat_value_t zero_fill(mat_t* M)
+{
+	const mat_value_t zero = {};
+	return zero;
+}
+
+static size_t value_size(mat_t* M)
+{
+	switch(M->type)
+	{
+		case d64:
+			return sizeof(double);
+		case f32:
+		default:
+			return sizeof(float);
+	}
+}
+
+int nn_mat_init(mat_t* M)
+{
+	if (!M) return -1;
+	if (!M->fill)
+	{
+		M->fill = zero_fill;
+	}
+
+	// determine the dimensionality asked for
+	for (M->_size = 0; M->_size < NN_MAT_MAX_DIMS && M->dims[M->_size];)
+	{
+		++M->_size;
+	}
+
+	// size less than 2 is no good
+	assert(M->_size >= 2);
+
+	size_t total_elements = M->dims[0];
+	for (int i = 1; i < M->_size; ++i)
+	{
+		total_elements *= M->dims[i];
+	}
+
+	M->_data = calloc(total_elements, value_size(M));
+
+	// Check for allocation failure
+	if (!M->_data) return -2;
+
+	// perform fill initialization
+	for (int i = total_elements; i--;)
+	{
+		switch(M->type)
+		{
+			case f32:
+				((float*)M->_data)[i] = M->fill(M).f;
+				break;
+			case d64:
+				((double*)M->_data)[i] = M->fill(M).d;
+				break;
+		}
+	}
+
+	return 0;
+}
+
+#define e2f(M, i, j) (((float*)(M)->_data) + (M)->dims[1] * i + j)
+#define e2d(M, i, j) (((double*)(M)->_data) + (M)->dims[1] * i + j)
+
+void nn_mat_mul(mat_t* R, mat_t* A, mat_t* B)
+{
+	// MxN * NxO = MxO
+	
+	assert(R._size == A._size);
+	assert(A._size == B._size);
+	assert(A.dims[1] == B.dims[0]);
+
+	for (int ar = A.dims[0]; ar--;)
+	for (int bc = B.dims[1]; bc--;)
+	{
+		float dot = 0;
+		for (int i = B.dims[0]; i--;)
+		{
+			dot += e2f(A, ar, i) * e2f(B, i, bc);
+		}
+
+		e2f(R, ar, bc) = dot;
+	}
+}
---
+++ ml/recognizer/src/nn.h
@@ -0,0 +1,94 @@
+#ifndef AVC_NN
+#define AVC_NN
+
+#define NN_MAT_MAX_DIMS 4
+
+typedef enum {
+	f32 = 0,
+	d64
+} mat_storage_t;
+
+typedef union {
+	float f;
+	double d;
+} mat_value_t;
+
+struct mat_t;
+//typedef mat_value_t (*mat_initializer)(struct mat_t*);
+
+struct mat_t {
+	/**
+	 * @brief Type used to represent numeric values in the matrix
+	 */
+	mat_storage_t type;
+
+	/**
+	 * @brief Int array specifying the length of each
+	 *        orthoganal dimension. Must be null terminated
+	 */
+	int dims[NN_MAT_MAX_DIMS];
+	
+	/**
+	 * @brief Optional: Uses this function to initialize each value
+	 *                  of the matrix
+	 */
+	mat_value_t (*fill)(struct mat_t*);
+
+	/**
+	 * @brief number of dimensions, this will be filled in automatically
+	 */
+	unsigned int _size;
+
+	/**
+	 * @brief Raw pointer to a contiguous array used
+	 *        to store the matrix's data
+	 */
+	void* _data;
+};
+typedef struct mat_t mat_t;
+
+/**
+ * @brief Allocates memory for matrix described by 'M'
+ * @param M - description of desired matrix
+ * @return 0 on success.
+ **/
+int nn_mat_init(mat_t* M);
+
+// Matrix operations
+// ------------------------------------
+// All of these functions implicitly succeed, errors or inconsistencies
+// will cause program termination
+
+/**
+ * @brief Performs matrix multiplication A x B storing the result in R
+ * @param R - Resulting matrix of the multiplication. It's dimensions must be valid
+ * @param A - Left hand of the muliplication
+ * @param B - Right hand of the multiplication
+ */
+void nn_mat_mul(mat_t* R, mat_t* A, mat_t* B);
+
+/**
+ * @brief Performs element-wise multiplication A x B storing the result in R
+ * @param R - Resulting matrix of the multiplication. It's dimensions must be valid
+ * @param A - Left hand of the muliplication
+ * @param B - Right hand of the multiplication
+ */
+void nn_mat_mul_e(mat_t* R, mat_t* A, mat_t* B);
+
+/**
+ * @brief Performs scaling operation on whole matrix M storing the result in R
+ * @param R - Resulting matrix of the multiplication. It's dimensions must be valid
+ * @param M - Left hand of the muliplication
+ * @param s - Scalar that is multiplied by each element of M
+ */
+void nn_mat_scl_e(mat_t* R, mat_t* M, mat_value_t s);
+
+/**
+ * @brief Performs element-wise addition A + B storing the result in R
+ * @param R - Resulting matrix of the addition. It's dimensions must be valid
+ * @param A - Left hand of the addition
+ * @param B - Right hand of the addition 
+ */
+void nn_mat_add_e(mat_t* R, mat_t* A, mat_t* B);
+
+#endif
